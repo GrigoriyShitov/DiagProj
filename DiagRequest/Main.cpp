0235bca2f4643a6219bb39a995dd56f6999ffee9
GrigoriyShitov <grisha.shitob1@gmail.com>
2023-11-27 16:27:18 +0300

    Finally added gsm

--- DiagRequest/Main.cpp
+++ DiagRequest/Main.cpp
@@ -19,7 +19,7 @@ int main()
 	bool iret;
 
 	uint8_t payload = 0;//var for check payload length
-	uint8_t buffer[100];//buffer for string which needs to separate format string in pack method
+	uint8_t buffer[1000];//buffer for string which needs to separate format string in pack method
 
 	memset(read_buffer, 0, bufSize);//zeroing buffers
 	memset(write_buffer, 0, bufSize);//
@@ -27,29 +27,48 @@ int main()
 	std::thread t0(CeMain, std::ref(SimPort),std::ref(q));
 	q.waitData();
 	q.popN();
-	int32_t a = 0, c=0; //***  need to change this variables to define value of config op number
-	uint8_t b=5;
+	int32_t a, c; //***  need to change this variables to define value of config op number
+	uint8_t b;
 	uint32_t size = 0;
 	uint8_t opNum = 0;
 	while (1) {
 		switch (opNum)
 		{
 		case 0:
-			payload = StructPack::pack(buffer, 100, "3xi", a);
+			a = 0;
+			payload = StructPack::pack(buffer, 1000, "3xi", a);
 			size = Payload::SetPayload(DIAG_LOG_CONFIG_F, buffer, payload, write_buffer);
 
 			iret = SimPort.SendData(write_buffer, size);
 			opNum++;
 			break;
 		case 1:
-			payload = StructPack::pack(buffer, 100, "BxxI", b, c);
+			b = 5;
+			c = 0;
+			payload = StructPack::pack(buffer, 1000, "BxxI", b, c);
 			size = Payload::SetPayload(DIAG_EXT_MSG_CONFIG_F, buffer, payload, write_buffer);
 			iret = SimPort.SendData(write_buffer, size);
 			opNum++;
 			break;
 		case 2:
+			b = 1;
+			payload = StructPack::pack(buffer, 1000, "3xI", b);
+			size = Payload::SetPayload(DIAG_LOG_CONFIG_F, buffer, payload, write_buffer);
+			iret = SimPort.SendData(write_buffer, size);
+			opNum++;
+			break;
+		case 3:
+			a = 0;
 			b = 0;
-			payload = StructPack::pack(buffer, 100, "3xIIBIIIIIIIIIIIIIIIIBBB", LOG_CONFIG_SET_MASK_OP, '\r', 255, 1, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, b, b, b);
+			payload = StructPack::pack(buffer, 1000, "3xIIBB39xB30xB63x", LOG_CONFIG_SET_MASK_OP, 0x5, ' ',0x4, 0x80,'@');//GSM s
+			size = Payload::SetPayload(DIAG_LOG_CONFIG_F, buffer, payload, write_buffer);
+			iret = SimPort.SendData(write_buffer, size);
+			opNum++;
+			break;
+		case 4:
+			a = 0;
+			b = 0;
+			payload = StructPack::pack(buffer, 1000, "3xIIBIIIIIIIIIIIIIIIIBBB", LOG_CONFIG_SET_MASK_OP, '\r', 255, 1, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, b, b, b);
 			size = Payload::SetPayload(DIAG_LOG_CONFIG_F, buffer, payload, write_buffer);
 			iret = SimPort.SendData(write_buffer, size);
 			opNum++;
